c46e481672dcd02b4c11fc559f090a33d107a99c
gurung10135 <santosh.grg009870@gmail.com>
2026-05-19 10:30:20 +0000

    change

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import router from "./routes/index";
+import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -2,7 +2,6 @@ import { Router, type IRouter } from "express";
 import healthRouter from "./health";
 
 const router: IRouter = Router();
-
 router.use(healthRouter);
 
 export default router;
