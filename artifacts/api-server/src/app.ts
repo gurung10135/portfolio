a15d004b08c3a4d937eca26e89b85d8462f780d7
gurung10135 <santosh.grg009870@gmail.com>
2026-05-19 09:41:30 +0000

    change

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import pinoHttp = require("pino-http");
 import router from "./routes";
 import { logger } from "./lib/logger";
 
@@ -10,20 +10,20 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
       },
     },
-  }),
+  })
 );
 app.use(cors());
 app.use(express.json());
