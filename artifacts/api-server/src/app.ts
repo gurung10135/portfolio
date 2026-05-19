c7f20cf6f826e617149980c8fea23d81ad86e49f
gurung10135 <santosh.grg009870@gmail.com>
2026-05-19 09:47:37 +0000

    ssaf

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,30 +1,12 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp = require("pino-http");
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
+const httpLogger = (require("pino-http") as any)({ logger });
 
-app.use(
-  pinoHttp({
-    logger,
-    serializers: {
-      req(req: any) {
-        return {
-          id: req.id,
-          method: req.method,
-          url: req.url?.split("?")[0],
-        };
-      },
-      res(res: any) {
-        return {
-          statusCode: res.statusCode,
-        };
-      },
-    },
-  })
-);
+app.use(httpLogger);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
